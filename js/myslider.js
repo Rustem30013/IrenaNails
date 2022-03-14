69320dcde26bc8ee0c570260010b22f68688486a
Rustem30013 <78695500+Rustem30013@users.noreply.github.com>
2022-03-15 00:08:41 +0300

    Add files via upload

---
+++ js/myslider.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.slidertwo').bxSlider(
+        {
+        auto:true,
+        autoHover:true,
+        shrinkItems:true,
+        infiniteLoop:true,
+        stopAutoOnClick:true,
+        speed:500,
+        pause:1500,
+        mode: 'fade',
+        }
+    );
+    $('.sliderthree').bxSlider({
+        auto:true,
+        autoHover:true,
+        shrinkItems:true,
+        infiniteLoop:true,
+        stopAutoOnClick:true,
+        speed:500,
+        pause:1500,
+        mode: 'fade',
+    });
+//    $('td').addClass( 'd-flex' );
+});
